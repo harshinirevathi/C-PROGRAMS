3c041ea58c538e7dd6cfb0ee9fc82f6eb6fdccb9
harshinirevathi <47887121+harshinirevathi@users.noreply.github.com>
2019-02-27 16:24:32 +0530

    Create SET1_5.C

---
+++ SET1_5.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b, c;
+    printf("enter the value a,b,c");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b&&a>c)
+    {
+        printf("a is greater");
+    }
+    else if(b>a&&b>c)
+    {
+        printf("b is greater");
+    }
+    else
+    {
+        printf("c is greater");
+    }
+    return 0;
+}
