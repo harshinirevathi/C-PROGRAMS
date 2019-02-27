beeb05d73e3566f09254283a4aaf8f30ea5308b0
harshinirevathi <47887121+harshinirevathi@users.noreply.github.com>
2019-02-27 15:47:35 +0530

    Create SET1-1-1.c

---
+++ SET1-1-1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("enter the value n");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("positive");
+    }
+    else if(n<0)
+    {
+        printf("negative");
+    }
+    else
+    {
+        printf("zero");
+    }
+
+    return 0;
+}
