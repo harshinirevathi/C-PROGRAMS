3f53f2b4def726927c96e2186ae9b9b03b937adb
harshinirevathi <47887121+harshinirevathi@users.noreply.github.com>
2019-02-27 15:53:44 +0530

    Create SET1-2.c

---
+++ SET1-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("enter the value n");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    return 0;
+}
