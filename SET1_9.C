ca5c8c6032054cb34fb730292cb6c0477cf9696c
harshinirevathi <47887121+harshinirevathi@users.noreply.github.com>
2019-03-01 16:01:51 +0530

    Rename SET1_10.C to SET1_9.C

---
+++ SET1_9.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int i,j,k,n,j=0,a[n];
+scan("&n&k",n,k);
+for(i=1;i<=n;i++)
+scanf("%d",&a[i]);
+for(i=1;i<k;i++)
+j=j+k;
+printf("the first num is %d num is %d",k,j); 
+getch();
+}
