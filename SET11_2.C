64dff41fae0f29d12364d108ae85511d573d18be
harshinirevathi <47887121+harshinirevathi@users.noreply.github.com>
2019-04-01 15:44:57 +0530

    Create SET11_2.C

---
+++ SET11_2.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    int n,t;
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+    t=n/2;
+    printf("%d",t);
+    }
+    else
+    {
+        printf("%d",n);
+    }
+getch();
+}
