d155dd69f78c3d9e2d1a6874f4007520aa884c66
harshinirevathi <47887121+harshinirevathi@users.noreply.github.com>
2019-04-03 15:44:26 +0530

    Create PSET2_1.C

---
+++ PSET2_1.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[100];
+    int i;
+    printf("enter the character");
+    scanf("%s",a);
+    {
+    if(a[0]=='s')
+    {
+        printf("yes");
+    }
+    else
+    {
+    printf("no");
+    }
+    }
+return 0;
+}
